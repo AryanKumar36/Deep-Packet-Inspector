2fe32a24b897ab092dc8b550c260871c10f0dba3
AryanKumar36 <ak24012000@gmail.com>
2026-02-27 13:55:45 +0530

    Spring Initializer

---
+++ dpi-engine/src/main/java/com/aryan/dpi_engine/DpiEngineApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.dpi_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpiEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpiEngineApplication.class, args);
+	}
+
+}
---
+++ dpi-engine/src/test/java/com/aryan/dpi_engine/DpiEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryan.dpi_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpiEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
